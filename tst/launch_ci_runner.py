457c70f8ba4940dc561f96d6e5f61f5024689397
Ben Ryan <brryan@lanl.gov>
2024-12-04 11:44:36 -0700

    Fix to dir group/permission for darwin CI

--- tst/launch_ci_runner.py
+++ tst/launch_ci_runner.py
@@ -96,16 +96,22 @@ def run_tests_in_temp_dir(pr_number, head_repo, head_ref, output_dir):
         ]
         ret = subprocess.run(test_command, check=True)
 
+        # Set group ownership
+        subprocess.run(
+           ["chgrp", "-R", "jovian", output_dir],
+           check=True,
+        )
+
         # Set permissions for directories
         subprocess.run(
-            ["find", output_dir, "-type", "d", "-exec", "chmod", "750", "{}", "+"],
-            check=True,
+           ["find", output_dir, "-type", "d", "-exec", "chmod", "770", "{}", "+"],
+           check=True,
         )
 
         # Set permissions for files
         subprocess.run(
-            ["find", output_dir, "-type", "f", "-exec", "chmod", "640", "{}", "+"],
-            check=True,
+           ["find", output_dir, "-type", "f", "-exec", "chmod", "660", "{}", "+"],
+           check=True,
         )
 
         # Return true if the test script succeeded

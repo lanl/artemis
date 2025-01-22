ea5f638b716b579fb971d76d25a98723b1b3bcf0
Adam Dempsey <adempsey@lanl.gov>
2025-01-22 14:46:27 -0700

    Remove unphysical opacity models. Add powerlaw model

--- src/gas/gas.cpp
+++ src/gas/gas.cpp
@@ -139,15 +139,14 @@ std::shared_ptr<StateDescriptor> Initialize(ParameterInput *pin,
   } else if (opacity_model_name == "constant") {
     const Real kappa_a = pin->GetOrAddReal("gas/opacity/absorption", "kappa_a", 0.0);
     opacity = NonCGSUnits<Gray>(Gray(kappa_a), time, mass, length, temp);
-  } else if (opacity_model_name == "shocktube_a") {
+  } else if (opacity_model_name == "powerlaw") {
     const Real coef_kappa_a =
         pin->GetOrAddReal("gas/opacity/absorption", "coef_kappa_a", 0.0);
     const Real rho_exp = pin->GetOrAddReal("gas/opacity/absorption", "rho_exp", 0.0);
     const Real temp_exp = pin->GetOrAddReal("gas/opacity/absorption", "temp_exp", 0.0);
-    opacity = ArtemisUtils::ShocktubeAOpacity(coef_kappa_a, rho_exp, temp_exp);
-  } else if (opacity_model_name == "thermalization") {
-    const Real kappa_a = pin->GetOrAddReal("gas/opacity/absorption", "kappa_a", 0.0);
-    opacity = ArtemisUtils::ThermalizationOpacity(kappa_a);
+
+    opacity = NonCGSUnits<PowerLaw>(PowerLaw(coef_kappa_a, rho_exp, temp_exp), time, mass,
+                                    length, temp);
   } else {
     PARTHENON_FAIL("Opacity model not recognized!");
   }

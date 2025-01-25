d028edf1b6619a1dbc93a5d173d0d99f678b1578
Adam Dempsey <adempsey@lanl.gov>
2025-01-25 13:29:04 -0700

    Only error out if we are not converged

--- src/radiation/moment/matter_coupling.hpp
+++ src/radiation/moment/matter_coupling.hpp
@@ -230,9 +230,9 @@ TaskStatus MatterCouplingImpl(MeshData<Real> *u0, MeshData<Real> *u1, const Real
                   // Ek += c / chat * dE;
 
                   inner_iter++;
-                  inner_conv = (inner_err < inner_tol) || (inner_iter > inner_max);
+                  inner_conv = (inner_err <= inner_tol) || (inner_iter > inner_max);
                 }
-                if (inner_iter > inner_max) {
+                if ((inner_iter > inner_max) && (inner_err > inner_tol)) {
                   printf("No inner convergence after %d iterations: %lg > %lg\n",
                          inner_iter, inner_err, inner_tol);
                   PARTHENON_FAIL("");
@@ -390,7 +390,7 @@ TaskStatus MatterCouplingImpl(MeshData<Real> *u0, MeshData<Real> *u1, const Real
                 outer_iter++;
                 outer_conv = (outer_err < outer_tol) || (outer_iter > outer_max);
               }
-              if (outer_iter > outer_max) {
+              if ((outer_iter > outer_max) && (outer_err > outer_tol)) {
                 printf("No outer convergence after %d iterations: %lg > %lg\n",
                        outer_iter, outer_err, outer_tol);
                 PARTHENON_FAIL("");

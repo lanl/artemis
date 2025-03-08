97aa1d3c56658c5b49bb4369a518bcf3bef54c80
Adam Dempsey <adempsey@lanl.gov>
2025-03-08 10:29:50 -0700

    Move BYTE declaration to artemis.hpp. Move read/write file utils to nbody_utils

--- src/nbody/nbody.cpp
+++ src/nbody/nbody.cpp
@@ -27,12 +27,6 @@ extern "C" {
 #include "nbody/nbody_utils.hpp"
 #include "utils/units.hpp"
 
-#ifdef PORTABLE_RESTART
-using BINARY_CHAR = uint8_t;
-#else
-using BINARY_CHAR = char;
-#endif
-
 using parthenon::MetadataFlag;
 
 namespace NBody {
@@ -143,7 +137,7 @@ std::shared_ptr<StateDescriptor> Initialize(ParameterInput *pin,
   params.Add("particle_force_tot", particle_force_tot);
 
   // Create vector for Rebound restart
-  std::vector<BINARY_CHAR> reb_sim_restart;
+  std::vector<BYTE> reb_sim_restart;
   params.Add("reb_sim_buffer", reb_sim_restart, Params::Mutability::Restart);
 
   // Output parameters
@@ -347,15 +341,12 @@ void UserWorkBeforeRestartOutputMesh(Mesh *pmesh, ParameterInput *, SimTime &,
 #endif
 
   // Read Rebound restart back into string
-  std::ifstream file(NBody::rebound_filename, std::ios::binary);
-  PARTHENON_REQUIRE(file.is_open(), "Error opening temporary rebound output file!");
-  std::vector<BINARY_CHAR> reb_sim_buffer((std::istreambuf_iterator<char>(file)),
-                                          std::istreambuf_iterator<char>());
-  file.close();
+
+  auto reb_sim_buffer = read_bytes_from_file(NBody::rebound_filename);
 
   // Store current rebound output as restartable parameter.  Every rank must store a
   // matching buffer parameter or else I/O will hang
-  nbody_pkg->UpdateParam<std::vector<BINARY_CHAR>>("reb_sim_buffer", reb_sim_buffer);
+  nbody_pkg->UpdateParam<std::vector<BYTE>>("reb_sim_buffer", reb_sim_buffer);
 }
 
 //----------------------------------------------------------------------------------------
@@ -374,11 +365,9 @@ void InitializeFromRestart(Mesh *pm) {
   // Initialize rebound state on rank 0
   if (Globals::my_rank == 0) {
     // Create rebound save file from stored buffer
-    auto reb_sim_buffer = nbody_pkg->Param<std::vector<BINARY_CHAR>>("reb_sim_buffer");
-    std::ofstream outfile(NBody::rebound_filename.c_str(), std::ios::binary);
-    outfile.write(reinterpret_cast<const char *>(reb_sim_buffer.data()),
-                  reb_sim_buffer.size());
-    outfile.close();
+    auto reb_sim_buffer = nbody_pkg->Param<std::vector<BYTE>>("reb_sim_buffer");
+
+    write_bytes_to_file(NBody::rebound_filename, reb_sim_buffer);
 
     // Create rebound simulation from new save file
     RebSim new_reb_sim(NBody::rebound_filename);

09b8a2bb4aead890e2be6833cfc0a37e846d0afb
Patrick Mullen <pdmullen@pn2293621.lanl.gov>
2025-05-27 21:39:26 -0400

    Change drag CI tol

--- tst/scripts/drag/drag.py
+++ tst/scripts/drag/drag.py
@@ -126,5 +126,7 @@ def analyze():
 
     errors = np.array(errors).ravel()
     fail = np.any(errors > _tol)
-    fail |= np.max(mom_err) > 1e-13
+    # NOTE(@pdmullen): PR#74 reduces the number of divides, but this leads to differing
+    # round-off behavior... increment tolerance
+    fail |= np.max(mom_err) > 2e-13
     return not fail

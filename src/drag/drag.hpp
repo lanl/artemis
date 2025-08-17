8daec6dfb094dec5f1be37310a98ba0f16eb2922
Adam Dempsey <adempsey@lanl.gov>
2025-08-16 18:40:16 -0600

    use cached coords

--- src/drag/drag.hpp
+++ src/drag/drag.hpp
@@ -207,6 +207,9 @@ TaskStatus SelfDragSourceImpl(MeshData<Real> *md, const Real time, const Real dt
                          gas::cons::internal_energy, dust::cons::momentum,
                          dust::cons::density>(resolved_pkgs.get());
   auto vmesh = desc.GetPack(md);
+  static auto desc_g = MakePackDescriptor<geom::x1v, geom::x2v, geom::x3v, geom::hx1v,
+                                          geom::hx2v, geom::hx3v>(resolved_pkgs.get());
+  auto vg = desc_g.GetPack(md);
   const auto ib = md->GetBoundsI(IndexDomain::interior);
   const auto jb = md->GetBoundsJ(IndexDomain::interior);
   const auto kb = md->GetBoundsK(IndexDomain::interior);
@@ -217,8 +220,14 @@ TaskStatus SelfDragSourceImpl(MeshData<Real> *md, const Real time, const Real dt
       KOKKOS_LAMBDA(const int &b, const int &k, const int &j, const int &i) {
         // Extract coordinates
         geometry::Coords<GEOM> coords(cpars, vmesh.GetCoordinates(b), k, j, i);
-        const auto &xv = coords.GetCellCenter();
-        const auto &hx = coords.GetScaleFactors();
+        const std::array<Real, 3> xv{
+            vg(b, geom::x1v(), coords.template index<geom::x1v>(k, j, i)),
+            vg(b, geom::x2v(), coords.template index<geom::x2v>(k, j, i)),
+            vg(b, geom::x3v(), coords.template index<geom::x3v>(k, j, i))};
+        const std::array<Real, 3> hx{
+            vg(b, geom::hx1v(), coords.template index<geom::hx1v>(k, j, i)),
+            vg(b, geom::hx2v(), coords.template index<geom::hx2v>(k, j, i)),
+            vg(b, geom::hx3v(), coords.template index<geom::hx3v>(k, j, i))};
         const auto &[xcyl, ex1, ex2, ex3] = coords.ConvertToCylWithVec(xv);
 
         // Compute the (gas) ramp for this cell
@@ -375,6 +384,9 @@ TaskStatus SimpleDragSourceImpl(MeshData<Real> *md, const Real time, const Real 
                          gas::cons::internal_energy, dust::cons::momentum,
                          dust::cons::density>(resolved_pkgs.get());
   auto vmesh = desc.GetPack(md);
+  static auto desc_g = MakePackDescriptor<geom::x1v, geom::x2v, geom::x3v, geom::hx1v,
+                                          geom::hx2v, geom::hx3v>(resolved_pkgs.get());
+  auto vg = desc_g.GetPack(md);
   const auto ib = md->GetBoundsI(IndexDomain::interior);
   const auto jb = md->GetBoundsJ(IndexDomain::interior);
   const auto kb = md->GetBoundsK(IndexDomain::interior);
@@ -385,8 +397,14 @@ TaskStatus SimpleDragSourceImpl(MeshData<Real> *md, const Real time, const Real 
       KOKKOS_LAMBDA(const int &b, const int &k, const int &j, const int &i) {
         // Extract coordinates
         geometry::Coords<GEOM> coords(cpars, vmesh.GetCoordinates(b), k, j, i);
-        const auto &xv = coords.GetCellCenter();
-        const auto &hx = coords.GetScaleFactors();
+        const std::array<Real, 3> xv{
+            vg(b, geom::x1v(), coords.template index<geom::x1v>(k, j, i)),
+            vg(b, geom::x2v(), coords.template index<geom::x2v>(k, j, i)),
+            vg(b, geom::x3v(), coords.template index<geom::x3v>(k, j, i))};
+        const std::array<Real, 3> hx{
+            vg(b, geom::hx1v(), coords.template index<geom::hx1v>(k, j, i)),
+            vg(b, geom::hx2v(), coords.template index<geom::hx2v>(k, j, i)),
+            vg(b, geom::hx3v(), coords.template index<geom::hx3v>(k, j, i))};
         const auto &[xcyl, ex1, ex2, ex3] = coords.ConvertToCylWithVec(xv);
 
         // Compute the ramp for this cell

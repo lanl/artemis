bc30079922946bf5cad85b3fc93f54d9264e9818
Adam Dempsey <adempsey@lanl.gov>
2025-11-01 16:38:33 -0600

    Remove warnings and improve dPhidr

--- src/pgen/disk.hpp
+++ src/pgen/disk.hpp
@@ -69,6 +69,7 @@ struct DiskParams {
   bool nbody_temp;
   bool quiet_start;
   bool log;
+  bool multi_d, three_d;
 };
 
 struct State {
@@ -161,21 +162,28 @@ KOKKOS_INLINE_FUNCTION State ComputeDiskProfile(
   res.gdens = DenProfile(pgen, xcyl[0], xcyl[2]);
   const Real dxr = 1e-6 * std::sqrt(SQR(dx[0]) + SQR(dx[1]) + SQR(dx[2]));
 
-  const Real rt = pgen.nbody_temp
-                      ? -pgen.gm / Gravity::NBodyPotential<Coordinates::cylindrical>(
-                                       coords, xcyl, particles, npart)
-                      : xcyl[0];
+  Real rt = xcyl[0];
+  Real rtp = xcyl[0] + dxr;
+  Real rtm = xcyl[0] - dxr;
 
-  const Real rtp =
-      pgen.nbody_temp
-          ? -pgen.gm / Gravity::NBodyPotential<Coordinates::cylindrical>(
-                           coords, {xcyl[0] + dxr, xcyl[1], xcyl[2]}, particles, npart)
-          : xcyl[0] + dxr;
-  const Real rtm =
-      pgen.nbody_temp
-          ? -pgen.gm / Gravity::NBodyPotential<Coordinates::cylindrical>(
-                           coords, {xcyl[0] - dxr, xcyl[1], xcyl[2]}, particles, npart)
-          : xcyl[0] - dxr;
+  if (pgen.nbody_temp) {
+     const Real pot = Gravity::NBodyPotential<GEOM>(coords, xv, particles, npart);
+     const Real dxpot = Gravity::NBodyPotential<GEOM>(
+         coords, {xv[0] + 1e-6 * dx[0], xv[1], xv[2]}, particles, npart);
+     const Real dypot = Gravity::NBodyPotential<GEOM>(
+         coords, {xv[0], xv[1] + 1e-6 * dx[1], xv[2]}, particles, npart);
+     const Real dzpot = Gravity::NBodyPotential<GEOM>(
+         coords, {xv[0], xv[1], xv[2] + 1e-6 * dx[2]}, particles, npart);
+     // dPhi/dr = grad(Phi) . \hat{e}_r
+     Real drpot = (dxpot - pot) / (2e-6 * dx[0]) * ex1[0];
+     drpot += pgen.multi_d * (dypot - pot) / (2e-6 * dx[1]) * ex2[0];
+     drpot += pgen.three_d * (dzpot - pot) / (2e-6 * dx[2]) * ex3[0];
+     rt = -pgen.gm / pot;
+     rtp = -pgen.gm / (pot + drpot * dxr);
+     rtm = -pgen.gm / (pot - drpot * dxr);
+   }
+
+
   res.gtemp = TempProfile(pgen, rt, xcyl[2]);
   const Real tp = TempProfile(pgen, rtp, xcyl[2]);
   const Real tm = TempProfile(pgen, rtm, xcyl[2]);
@@ -259,6 +267,10 @@ inline void InitDiskParams(MeshBlock *pmb, ParameterInput *pin) {
         eos.PressureFromDensityInternalEnergy(disk_params.dens_min, disk_params.sie_min);
     disk_params.temp_min = eos.TemperatureFromDensityInternalEnergy(disk_params.dens_min,
                                                                     disk_params.sie_min);
+
+    const auto nx = params.Get<std::array<int, 3>>("prob_dim");
+    disk_params.three_d = nx[2] > 1;
+    disk_params.multi_d = disk_params.three_d || (nx[1] > 1);
 
     disk_params.do_gas = true; // NOTE(@pdmullen): Hardcoded for now...
     disk_params.do_dust = params.Get<bool>("do_dust");

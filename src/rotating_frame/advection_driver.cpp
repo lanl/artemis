c3ad0ba75b51252be15a3a5b9b9b81b71ab14a13
ajdittmann <dittmann@ias.edu>
2026-01-07 17:23:21 -0500

    update files conflicting with base

--- src/rotating_frame/advection_driver.cpp
+++ src/rotating_frame/advection_driver.cpp
@@ -11,123 +11,162 @@
 // the public, perform publicly and display publicly, and to permit others to do so.
 //========================================================================================
 
-// Parthenon includes
-#include <parthenon/driver.hpp>
-#include <parthenon/package.hpp>
-
 // Artemis includes
+#include "rotating_frame.hpp"
 #include "artemis.hpp"
-#include "derived/fill_derived.hpp"
-#include "geometry/geometry.hpp"
-#include "rotating_frame/rotating_frame.hpp"
+#include "rotating_frame_impl.hpp"
 #include "utils/artemis_utils.hpp"
-#include "utils/integrators/artemis_integrator.hpp"
-
-using namespace parthenon::driver::prelude;
-using namespace parthenon::package::prelude;
 
 namespace RotatingFrame {
 
 //----------------------------------------------------------------------------------------
-//! \fn TaskListStatus RotatingFrame::Advect
-//! \brief Executes linear advection term for orbital advection
-TaskListStatus Advect(Mesh *pmesh, const SimTime &tm) {
-  // Craft a series of **equal** subsetps that sum to the unsplit step
-  const Real dtlimit = EstimateTimestep(pmesh, 1.0);
-  const int nsteps = static_cast<int>(std::ceil(tm.dt / dtlimit));
-  const Real scdt = tm.dt / nsteps;
+//! \fn  StateDescriptor RotatingFrame::Initialize
+//! \brief Adds intialization function for rotating frame package
+std::shared_ptr<StateDescriptor> Initialize(ParameterInput *pin) {
+  auto rframe_pkg = std::make_shared<StateDescriptor>("rotating_frame");
+  Params &params = rframe_pkg->AllParams();
 
-  // Report number of substeps
-  if (tm.ncycle % tm.ncycle_out == 0) {
-    if (Globals::my_rank == 0) {
-      std::cout << "(Linear Advection) "
-                << "Executing " << nsteps << " substeps"
-                << " with dt=" << scdt << std::endl;
-    }
+  const Real omega = pin->GetReal("rotating_frame", "omega");
+  const Real qshear = pin->GetOrAddReal("rotating_frame", "qshear", 0.0);
+  PARTHENON_REQUIRE(omega != 0.0, "rotating_frame/omega cannot be zero! To disable, set "
+                                  "physics/rotating_frame = false");
+  if (qshear != 0) {
+    const std::string sys = pin->GetString("artemis", "coordinates");
+    PARTHENON_REQUIRE(
+        sys == "cartesian",
+        "rotating_frame/qshear must be zero for non-Cartesian coordinate systems!");
+    PARTHENON_REQUIRE(parthenon::Globals::nghost >= 2,
+                      "Rotating frame advection step requires at least 2 ghost cells.");
   }
+  params.Add("omega", omega);
+  params.Add("qshear", qshear);
 
-  // Execute LinearAdvectionStep over substeps
-  for (int step = 1; step <= nsteps; step++) {
-    auto status = LinearAdvectionStep(pmesh, tm, scdt).Execute();
-    if (status != TaskListStatus::complete) return status;
-  }
+  // Linear advection timestep controls
+  const Real cfl = pin->GetOrAddReal("rotating_frame", "cfl", 0.9);
+  const Real dt_ratio = pin->GetOrAddReal("rotating_frame", "dt_ratio", 100.0);
+  params.Add("cfl", cfl);
+  params.Add("dt_ratio", dt_ratio);
 
-  return TaskListStatus::complete;
-}
+  // Reconstruction algorithm for remap
+  ReconstructionMethod recon_method = ReconstructionMethod::null;
+  const std::string recon = pin->GetOrAddString("rotating_frame", "reconstruct", "plm");
+  recon_method = ArtemisUtils::ChooseReconMethod(recon);
+  params.Add("recon", recon_method);
 
-//----------------------------------------------------------------------------------------
-//! \fn  TaskCollection LinearAdvectionStep
-TaskCollection LinearAdvectionStep(Mesh *pmesh, const SimTime &tm, const Real scdt) {
-  TaskCollection tc;
-  if (!(pmesh->ndim >= 2)) return tc;
+  // Coordinates
+  const int ndim = ProblemDimension(pin);
+  std::string sys = pin->GetOrAddString("artemis", "coordinates", "cartesian");
+  Coordinates coords = geometry::CoordSelect(sys, ndim);
+  params.Add("coords", coords);
 
-  // Construct TaskCollection
-  using namespace ::parthenon::Update;
-  TaskID none(0);
-  const auto any = parthenon::BoundaryType::any;
-  const int num_partitions = pmesh->DefaultNumPartitions();
-
-  // Operator split linear advection
-  TaskRegion &tr = tc.AddRegion(num_partitions);
-  for (int i = 0; i < num_partitions; i++) {
-    auto &tl = tr[i];
-    auto &u0 = pmesh->mesh_data.GetOrAdd("u0", i);
-
-    auto start_recv = tl.AddTask(none, parthenon::StartReceiveBoundBufs<any>, u0);
-    auto update = tl.AddTask(start_recv, LagrangeRemap, u0.get(), scdt);
-    auto set_aux = tl.AddTask(
-        update, ArtemisDerived::SetAuxillaryFields<Coordinates::cartesian>, u0.get());
-    auto c2p = tl.AddTask(set_aux, PreCommFillDerived<MeshData<Real>>, u0.get());
-    auto bcs = parthenon::AddBoundaryExchangeTasks(c2p, tl, u0, pmesh->multilevel);
-    auto p2c = tl.AddTask(bcs, FillDerived<MeshData<Real>>, u0.get());
-  }
-
-  return tc;
-}
-
-//----------------------------------------------------------------------------------------
-//! \fn  TaskStatus RotatingFrame::LagrangeRemap
-//! \brief
-TaskStatus LagrangeRemap(MeshData<Real> *u0, const Real scdt) {
-  using parthenon::MakePackDescriptor;
-  auto pm = u0->GetParentPointer();
-  auto &resolved_pkgs = pm->resolved_packages;
-
-  // Artemis package and params
-  auto &artemis_pkg = pm->packages.Get("artemis");
-  const bool do_gas = artemis_pkg->template Param<bool>("do_gas");
-  const bool do_dust = artemis_pkg->template Param<bool>("do_dust");
-
-  // Rotating frame package and params
-  auto &rframe_pkg = pm->packages.Get("rotating_frame");
-  const Real qshear = rframe_pkg->template Param<Real>("qshear");
-  const Real om0 = rframe_pkg->template Param<Real>("omega");
-  const auto recon = rframe_pkg->template Param<ReconstructionMethod>("recon");
-
-  // Extract integrator weights
-  const Real dwdt = -qshear * om0 * scdt;
-
-  // Packing and indexing
-  static auto desc =
-      MakePackDescriptor<gas::cons::density, gas::cons::momentum, gas::cons::total_energy,
-                         gas::cons::internal_energy, dust::cons::density,
-                         dust::cons::momentum>(resolved_pkgs.get());
-  auto v0 = desc.GetPack(u0);
-
-  // Call upwind advection routines with requested recon
-  if (recon == ReconstructionMethod::pcm) {
-    return LagrangeRemapImpl<ReconstructionMethod::pcm>(u0, v0, dwdt);
-  } else if (recon == ReconstructionMethod::plm) {
-    return LagrangeRemapImpl<ReconstructionMethod::plm>(u0, v0, dwdt);
-  } else if (recon == ReconstructionMethod::ppm) {
-    return LagrangeRemapImpl<ReconstructionMethod::ppm>(u0, v0, dwdt);
-  } else if (recon == ReconstructionMethod::wenoz) {
-    return LagrangeRemapImpl<ReconstructionMethod::wenoz>(u0, v0, dwdt);
+  // Rotating frame timestep (if do_shear)
+  if (coords == Coordinates::cartesian) {
+    rframe_pkg->EstimateTimestepMesh = EstimateTimestepMesh<Coordinates::cartesian>;
+  } else if (coords == Coordinates::spherical1D) {
+    rframe_pkg->EstimateTimestepMesh = EstimateTimestepMesh<Coordinates::spherical1D>;
+  } else if (coords == Coordinates::spherical2D) {
+    rframe_pkg->EstimateTimestepMesh = EstimateTimestepMesh<Coordinates::spherical2D>;
+  } else if (coords == Coordinates::spherical3D) {
+    rframe_pkg->EstimateTimestepMesh = EstimateTimestepMesh<Coordinates::spherical3D>;
+  } else if (coords == Coordinates::cylindrical) {
+    rframe_pkg->EstimateTimestepMesh = EstimateTimestepMesh<Coordinates::cylindrical>;
+  } else if (coords == Coordinates::axisymmetric) {
+    rframe_pkg->EstimateTimestepMesh = EstimateTimestepMesh<Coordinates::axisymmetric>;
   } else {
-    PARTHENON_FAIL("Unsupported reconstruction method in rotating_frame");
+    PARTHENON_FAIL("Invalid artemis/coordinate system!");
+  }
+
+  return rframe_pkg;
+}
+
+//----------------------------------------------------------------------------------------
+//! \fn  TaskStatus RotatingFrame::RotatingFrameForce
+//! \brief
+TaskStatus RotatingFrameForce(MeshData<Real> *md, const Real time, const Real dt) {
+  PARTHENON_INSTRUMENT
+  using parthenon::MakePackDescriptor;
+  using TE = parthenon::TopologicalElement;
+  auto pm = md->GetParentPointer();
+
+  auto &artemis_pkg = pm->packages.Get("artemis");
+  const bool do_gas = artemis_pkg->Param<bool>("do_gas");
+  const bool do_dust = artemis_pkg->Param<bool>("do_dust");
+  const auto coords = artemis_pkg->Param<Coordinates>("coords");
+
+  auto &rframe_pkg = pm->packages.Get("rotating_frame");
+  const Real om0 = rframe_pkg->Param<Real>("omega");
+  const Real qshear = rframe_pkg->Param<Real>("qshear");
+
+  // Switch for the different implementations based on coordinate system
+  if (coords == Coordinates::cartesian) {
+    return ShearingBoxImpl(md, om0, qshear, do_gas, do_dust, dt);
+  } else if (coords == Coordinates::axisymmetric) {
+    return RotatingFrameImpl<Coordinates::axisymmetric>(md, om0, do_gas, do_dust, dt);
+  } else if (coords == Coordinates::spherical1D) {
+    return RotatingFrameImpl<Coordinates::spherical1D>(md, om0, do_gas, do_dust, dt);
+  } else if (coords == Coordinates::spherical2D) {
+    return RotatingFrameImpl<Coordinates::spherical2D>(md, om0, do_gas, do_dust, dt);
+  } else if (coords == Coordinates::spherical3D) {
+    return RotatingFrameImpl<Coordinates::spherical3D>(md, om0, do_gas, do_dust, dt);
+  } else if (coords == Coordinates::cylindrical) {
+    return RotatingFrameImpl<Coordinates::cylindrical>(md, om0, do_gas, do_dust, dt);
+  } else {
+    PARTHENON_FAIL("Rotating frame is not consistent with this coordinate system");
   }
 
   return TaskStatus::complete;
 }
+
+//----------------------------------------------------------------------------------------
+//! \fn Real RotatingFrame::EstimateTimestep
+//! \brief Compute multiple of linear advection timestep (if do_shear)
+template <Coordinates GEOM>
+Real EstimateTimestepMesh(MeshData<Real> *md) {
+  PARTHENON_INSTRUMENT
+  auto pmesh = md->GetParentPointer();
+  const bool do_shear = pmesh->packages.Get("artemis")->template Param<bool>("do_shear");
+  if (!(do_shear)) return Big<Real>();
+
+  auto &rframe_pkg = pmesh->packages.Get("rotating_frame");
+  const Real &dt_ratio = rframe_pkg->template Param<Real>("dt_ratio");
+  return EstimateTimestep(pmesh, dt_ratio);
+}
+
+//----------------------------------------------------------------------------------------
+//! \fn Real RotatingFrame::EstimateTimeStep
+//! \brief Not enrolled in parthenon's determination for global dt
+Real EstimateTimestep(parthenon::Mesh *pmesh, const Real dt_ratio) {
+  PARTHENON_INSTRUMENT
+  // Extract rotating frame params
+  auto &rframe_pkg = pmesh->packages.Get("rotating_frame");
+  const Real &om0 = rframe_pkg->Param<Real>("omega");
+  const Real &qshear = rframe_pkg->Param<Real>("qshear");
+  const Real &cfl = rframe_pkg->template Param<Real>("cfl");
+
+  // Compute linear advection timestep to sub-cycle
+  Real min_dt = Big<Real>();
+  for (auto const &pmb : pmesh->block_list) {
+    const auto &reg = pmb->block_size;
+    const auto wp = BackgroundVelocity<Coordinates::cartesian>(qshear, om0, reg.xmax_[0]);
+    const auto wm = BackgroundVelocity<Coordinates::cartesian>(qshear, om0, reg.xmin_[0]);
+    const Real dx2 = (reg.xmax_[1] - reg.xmin_[1]) / reg.nx_[1];
+    min_dt = std::min(min_dt, dx2 / std::max(std::abs(wp[1]), std::abs(wm[1])));
+  }
+#ifdef MPI_PARALLEL
+  PARTHENON_MPI_CHECK(MPI_Allreduce(MPI_IN_PLACE, &min_dt, 1, MPI_PARTHENON_REAL, MPI_MIN,
+                                    MPI_COMM_WORLD));
+#endif
+  return cfl * min_dt * dt_ratio;
+}
+
+//----------------------------------------------------------------------------------------
+//! template instantiations
+typedef MeshData<Real> MD;
+template Real EstimateTimestepMesh<Coordinates::cartesian>(MD *md);
+template Real EstimateTimestepMesh<Coordinates::cylindrical>(MD *md);
+template Real EstimateTimestepMesh<Coordinates::spherical1D>(MD *md);
+template Real EstimateTimestepMesh<Coordinates::spherical2D>(MD *md);
+template Real EstimateTimestepMesh<Coordinates::spherical3D>(MD *md);
+template Real EstimateTimestepMesh<Coordinates::axisymmetric>(MD *md);
 
 } // namespace RotatingFrame

1ee7f01e8c3d0500448845db50c0dce95b5b2d57
ajdittmann <dittmann@ias.edu>
2026-03-16 11:12:14 -0400

    update advection_driver to LANL version

--- src/rotating_frame/advection_driver.cpp
+++ src/rotating_frame/advection_driver.cpp
@@ -11,11 +11,20 @@
 // the public, perform publicly and display publicly, and to permit others to do so.
 //========================================================================================
 
+// Parthenon includes
+#include <parthenon/driver.hpp>
+#include <parthenon/package.hpp>
+
 // Artemis includes
-#include "rotating_frame.hpp"
 #include "artemis.hpp"
-#include "rotating_frame_impl.hpp"
+#include "derived/fill_derived.hpp"
+#include "geometry/geometry.hpp"
+#include "rotating_frame/rotating_frame.hpp"
 #include "utils/artemis_utils.hpp"
+#include "utils/integrators/artemis_integrator.hpp"
+
+using namespace parthenon::driver::prelude;
+using namespace parthenon::package::prelude;
 
 namespace RotatingFrame {
 
@@ -75,23 +84,24 @@ TaskCollection LinearAdvectionStep(Mesh *pmesh, const SimTime &tm, const Real sc
     auto p2c = tl.AddTask(bcs, FillDerived<MeshData<Real>>, u0.get());
   }
 
-  return rframe_pkg;
+  return tc;
 }
 
 //----------------------------------------------------------------------------------------
-//! \fn  TaskStatus RotatingFrame::RotatingFrameForce
+//! \fn  TaskStatus RotatingFrame::LagrangeRemap
 //! \brief
 TaskStatus LagrangeRemap(MeshData<Real> *u0, const Real scdt) {
   PARTHENON_INSTRUMENT
   using parthenon::MakePackDescriptor;
-  using TE = parthenon::TopologicalElement;
-  auto pm = md->GetParentPointer();
+  auto pm = u0->GetParentPointer();
+  auto &resolved_pkgs = pm->resolved_packages;
 
+  // Artemis package and params
   auto &artemis_pkg = pm->packages.Get("artemis");
-  const bool do_gas = artemis_pkg->Param<bool>("do_gas");
-  const bool do_dust = artemis_pkg->Param<bool>("do_dust");
-  const auto coords = artemis_pkg->Param<Coordinates>("coords");
+  const bool do_gas = artemis_pkg->template Param<bool>("do_gas");
+  const bool do_dust = artemis_pkg->template Param<bool>("do_dust");
 
+  // Rotating frame package and params
   auto &rframe_pkg = pm->packages.Get("rotating_frame");
   const Real qshear = rframe_pkg->template Param<Real>("qshear");
   const Real om0 = rframe_pkg->template Param<Real>("omega");
@@ -119,62 +129,10 @@ TaskStatus LagrangeRemap(MeshData<Real> *u0, const Real scdt) {
   } else if (recon == ReconstructionMethod::ppm) {
     return LagrangeRemapImpl<ReconstructionMethod::ppm>(u0, v0, vg, dwdt);
   } else {
-    PARTHENON_FAIL("Rotating frame is not consistent with this coordinate system");
+    PARTHENON_FAIL("Unsupported reconstruction method in rotating_frame");
   }
 
   return TaskStatus::complete;
 }
-
-//----------------------------------------------------------------------------------------
-//! \fn Real RotatingFrame::EstimateTimestep
-//! \brief Compute multiple of linear advection timestep (if do_shear)
-template <Coordinates GEOM>
-Real EstimateTimestepMesh(MeshData<Real> *md) {
-  PARTHENON_INSTRUMENT
-  auto pmesh = md->GetParentPointer();
-  const bool do_shear = pmesh->packages.Get("artemis")->template Param<bool>("do_shear");
-  if (!(do_shear)) return Big<Real>();
-
-  auto &rframe_pkg = pmesh->packages.Get("rotating_frame");
-  const Real &dt_ratio = rframe_pkg->template Param<Real>("dt_ratio");
-  return EstimateTimestep(pmesh, dt_ratio);
-}
-
-//----------------------------------------------------------------------------------------
-//! \fn Real RotatingFrame::EstimateTimeStep
-//! \brief Not enrolled in parthenon's determination for global dt
-Real EstimateTimestep(parthenon::Mesh *pmesh, const Real dt_ratio) {
-  PARTHENON_INSTRUMENT
-  // Extract rotating frame params
-  auto &rframe_pkg = pmesh->packages.Get("rotating_frame");
-  const Real &om0 = rframe_pkg->Param<Real>("omega");
-  const Real &qshear = rframe_pkg->Param<Real>("qshear");
-  const Real &cfl = rframe_pkg->template Param<Real>("cfl");
-
-  // Compute linear advection timestep to sub-cycle
-  Real min_dt = Big<Real>();
-  for (auto const &pmb : pmesh->block_list) {
-    const auto &reg = pmb->block_size;
-    const auto wp = BackgroundVelocity<Coordinates::cartesian>(qshear, om0, reg.xmax_[0]);
-    const auto wm = BackgroundVelocity<Coordinates::cartesian>(qshear, om0, reg.xmin_[0]);
-    const Real dx2 = (reg.xmax_[1] - reg.xmin_[1]) / reg.nx_[1];
-    min_dt = std::min(min_dt, dx2 / std::max(std::abs(wp[1]), std::abs(wm[1])));
-  }
-#ifdef MPI_PARALLEL
-  PARTHENON_MPI_CHECK(MPI_Allreduce(MPI_IN_PLACE, &min_dt, 1, MPI_PARTHENON_REAL, MPI_MIN,
-                                    MPI_COMM_WORLD));
-#endif
-  return cfl * min_dt * dt_ratio;
-}
-
-//----------------------------------------------------------------------------------------
-//! template instantiations
-typedef MeshData<Real> MD;
-template Real EstimateTimestepMesh<Coordinates::cartesian>(MD *md);
-template Real EstimateTimestepMesh<Coordinates::cylindrical>(MD *md);
-template Real EstimateTimestepMesh<Coordinates::spherical1D>(MD *md);
-template Real EstimateTimestepMesh<Coordinates::spherical2D>(MD *md);
-template Real EstimateTimestepMesh<Coordinates::spherical3D>(MD *md);
-template Real EstimateTimestepMesh<Coordinates::axisymmetric>(MD *md);
 
 } // namespace RotatingFrame

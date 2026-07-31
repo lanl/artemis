d560dd0d5c354451a4dea38e2f89984fab4a6da9
Adam Dempsey <adempsey@lanl.gov>
2026-07-31 08:44:37 -0600

    Fix energy flux issue with mhd

--- src/utils/fluxes/riemann/llf.hpp
+++ src/utils/fluxes/riemann/llf.hpp
@@ -163,7 +163,10 @@ struct RiemannSolver<RSolver::llf, FLUID_TYPE, CTYPE,
                 vdBr = wr_ivx * wr_ibx + wr_ivy * wr_iby + wr_ivz * wr_ibz;
                 el += pbl;
                 er += pbr;
-                fsum_e -= (wl_ibx * vdBl + wr_ibx * vdBr) / mu0;
+                // E includes magnetic energy and the total pressure contains an equal
+                // magnetic-pressure contribution, so each state contributes 2 P_B v_x.
+                fsum_e += 2.0 * (pbl * wl_ivx + pbr * wr_ivx) -
+                           (wl_ibx * vdBl + wr_ibx * vdBr) / mu0;
                 fsum_by = (wl_ivx * wl_iby - wl_ivy * wl_ibx) +
                           (wr_ivx * wr_iby - wr_ivy * wr_ibx);
                 fsum_bz = (wl_ivx * wl_ibz - wl_ivz * wl_ibx) +

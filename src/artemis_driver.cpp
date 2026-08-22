369124c540e5501ef3bcff285b3c76216b8eae6a
Adam Dempsey <adempsey@lanl.gov>
2026-08-22 15:46:39 -0600

    Only look for rad_int if doing split radiation

--- src/artemis_driver.cpp
+++ src/artemis_driver.cpp
@@ -89,12 +89,12 @@ ArtemisDriver<GEOM>::ArtemisDriver(ParameterInput *pin, ApplicationInput *app_in
   do_moment_unsplit = false;
   do_moment_split = false;
   if (do_moment) {
-    auto rad_int = pin->GetOrAddString("radiation/moment", "integrator", "rk2");
-    PARTHENON_REQUIRE(((rad_int == "rk1") || (rad_int == "rk2") || (rad_int == "rk3")),
-                      "radiation/integrator must be rk1,rk2, or rk3.")
     do_moment_split = pin->GetOrAddBoolean("radiation/moment", "split", true);
     do_moment_unsplit = !do_moment_split;
     if (do_moment_split) {
+      auto rad_int = pin->GetOrAddString("radiation/moment", "integrator", "rk2");
+      PARTHENON_REQUIRE(((rad_int == "rk1") || (rad_int == "rk2") || (rad_int == "rk3")),
+                        "radiation/integrator must be rk1,rk2, or rk3.")
       rad_integrator = std::make_unique<Integrator_t>(rad_int);
     }
   }
